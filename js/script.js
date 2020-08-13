2587269ea32d390d275697802397dc2b0e61e565
will <will@mail.com>
2020-08-13 11:37:25 +0300

    second commit, work with timer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-// 1. Функция, которая будет скрывать ненужные табы
-
-// 2. Показать нужный таб
-
-// 3. Нужно назначить обработчик события на меню (Выберите стиль питания)
-// который и будет манипулировать этими функциями
-
+    // tabs 
 
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll(".tabcontent"),
@@ -48,4 +42,57 @@ window.addEventListener("DOMContentLoaded", () => {
             });
         }
     });
+
+    // timer 
+
+    const deadline = "2020-09-11";
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000*60*60*24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector("#days"),
+              hours = timer.querySelector("#hours"),
+              minutes = timer.querySelector("#minutes"),
+              seconds = timer.querySelector("#seconds"),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+              function updateClock() {
+                  const t = getTimeRemaining(endtime);
+
+                  days.innerHTML = getZero(t.days);
+                  hours.innerHTML = getZero(t.hours);
+                  minutes.innerHTML = getZero(t.minutes);
+                  seconds.innerHTML = getZero(t.seconds);
+
+                  if (t.total <= 0) {
+                      clearInterval(timeInterval);
+                  }
+              }
+    }
+
+    setClock(".timer", deadline);
 });
--- js/script1.js
+++ js/script1.js
@@ -35,4 +35,56 @@ window.addEventListener("DOMContentLoaded", () => {
 
     });
 
+    //timer
+
+    const deadline = "2020-09-11";
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        const days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor(t / (1000 * 60 * 60) % 24),
+              minutes = Math.floor(t / (1000 * 60) % 60),
+              seconds = Math.floor(t / 1000 % 60);
+        
+        return {
+            timestamp: t,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setTimer(selector, endtime) {
+        const timer = document.querySelector(selector);
+
+        const days = timer.querySelector("#days"),
+              hours = timer.querySelector("#hours"),
+              minutes = timer.querySelector("#minutes"),
+              seconds = timer.querySelector("#seconds");
+              interval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+        }
+    }
+
+    setTimer(".timer", deadline);
+
 });
